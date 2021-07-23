b3f0f0357407af05bb5a5f91cf806ea100b30ce9
jiazhichao <wit235813@gmail.com>
2021-07-23 16:52:51 +0800

    update

---
+++ publish.sh
@@ -0,0 +1,2 @@
+cd /Users/pangru/Documents/BlogPosts;git add .;git commit -m 'update';git push
+ssh root@45.63.114.236 "cd /opt/blog/content/ ; git reset --hard ;git pull;cd /opt/blog;hugo --buildFuture"
